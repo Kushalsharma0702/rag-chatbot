0adee576a2f771a47d5edcaa7c0cc34f95566690
Kushal Sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2025-05-16 05:48:23 +0530

    first commit

--- app.py
+++ app.py
@@ -133,3 +133,5 @@ def serve_static(path):
 
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
